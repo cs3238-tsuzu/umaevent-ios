aedfced1e2584d0d82abfd55a3ffd465b81fe8e1
Tsuzuki Tsuchiya <8574909+cs3238-tsuzu@users.noreply.github.com>
2021-05-15 23:45:33 +0900

    Initial Commit

---
+++ Shared/umaeventApp.swift
@@ -0,0 +1,17 @@
+//
+//  umaeventApp.swift
+//  Shared
+//
+//  Created by Tsuzu on 2021/05/15.
+//
+
+import SwiftUI
+
+@main
+struct umaeventApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
